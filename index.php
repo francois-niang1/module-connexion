216833f562bddb3fbe299a3d4883c6dd27c5cff3
francois-niang1 <francois.niang@laplateforme.io>
2021-11-16 15:38:05 +0100

    Page d'accueil presque fini

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/index.css" rel="stylesheet">
+        <title>Accueil</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navMenu">
+                <a href="index.php">Accueil</a>
+                <a href="inscription.php">Inscription</a>
+                <a href="connexion.php">Connexion</a>
+                <a href="admin.php">Admin</a>
+                <div class="dot"></div>
+            </nav>
+        </header>
+        <main>
+            <div class = 'Container'>
+                <div class="Part1">
+                    <div class="Left">
+                        <p>"Welcome In Takahiro World's" <br> Si tu es amateur de manga
+                            et d'art ce monde est fait pour toi !!! </p>
+                    </div>
+                    <div class="Right">
+                        
+                    </div>
+                </div>
+                <div class="Part2">
+                    <div class="Left">
+                        <p>Envie d'un poster ? <br> Personnaliser ?<br> 
+                        Une Coque ?<br> Un Cadre ?<br> Ou même une paire de chaussure ? <br> 
+                        Et bien bonne nouvelle !! Ne cherche plus tu es au bon endroit :) </p>
+                    </div>
+                    <div class="Right">
+                        
+                    </div>
+                </div>
+                <div class="Part1">
+                    <div class="Left">
+                        <p> Connecte toi pour profiter de reductions et points fidélités <br>
+                        Tu n'as pas de comptre ? c'est pas grave inscris toi juste ici ;) </p>
+                        <div class="DivButton">
+                            <button class='Button'><a class='Button' href="connexion.php">Connexion</a></button>
+                            <button class='Button'><a class='Button' href="inscription.php">Inscription</a></button>
+                        </div>
+                        
+                    </div>
+                    <div class="Right">
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="RightFooter">
+                Copyright © 2021 François Niang - All Rights Reserved
+            </div>
+            <div class="Center">
+
+            </div>
+            <div class="LeftFooter">
+                Suivre L'actualité de Takahiro
+                <a href="https://www.instagram.com/takahiro__arts/"><img class = 'LogoInsta' src="https://lh3.googleusercontent.com/proxy/yUFwQ9Zo066bT4xA0aQ8XnmVeoJtRSRjSx31IjYpffluFeE8HZSJXiyKRIbr915Dsr3sMbB9QBQCPz4pnIbCSg7Wd46nda94jzglvjiVcZeI8GmscE-jU8VnmUXWtEs3evxsIOInq3kRVGfM_7pGgjnrvxgJvg" alt="logo insta"></a> 
+                <img class = 'LogoSnap' src="http://assets.stickpng.com/images/5a4e30612da5ad73df7efe71.png" alt="logo snap">
+                <a href="https://www.jc-case.com/"><img class = 'LogoJC' src="https://www.jc-case.com/Files/130058/Img/10/logo-JC.png" alt="logo jc case"></a>
+            </div>
+        </footer>
+    </body>
+</html>
